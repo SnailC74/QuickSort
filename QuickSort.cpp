8ab2c8297277528324b2069686d624504b214ed7
SnailC74 <60499437+SnailC74@users.noreply.github.com>
2020-08-07 17:08:50 -0600

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int partition (int *a, int start, int end)
+int partition(int *a, int start, int end)
 {
   int pivot = a[end];
   int pivotIndex = start;
@@ -11,5 +11,44 @@ int partition (int *a, int start, int end)
       temp = a[i];
       a[i] = a[pivotIndex];
       a[pivotIndex] = temp;
+      pivotIndex++;
     }
-  }   
+  }
+  temp = a[end];
+  a[end] = a[pivotIndex];
+  a[pivotIndex] = temp;
+  
+  return pivotIndex;
+}
+
+void QuickSort(int *a, int start, int end)
+{
+  int pivotIndex;
+  if(start < end) {
+    pivotIndex = partition(a, start, end);
+    QuickSort(a, start, pivotIndex - 1);
+    QuickSort(a, pivotIndex + 1, end);
+  }
+}
+
+int main()
+{
+  int n, a[];
+  
+  cout << "Please enter the number of elements: ";
+  cihn >> n;
+  
+  cout << "Please enter the array elements: ";
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  
+  QuickSort(a, 0 ,n -1);
+  
+  cout << "After quick sort the array is: " << endl;
+  for(int i = 0; i < n; i++) {
+    cout <<  a[i];
+  }
+  return 0;
+}
+  }
